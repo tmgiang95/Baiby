08cdd54c7df4795ae90cc9672d2af03eb0921e19
giangtm1 <giangtm1@viettel.com.vn>
2018-12-10 16:59:29 +0700

    .test git

--- app/src/main/java/com/example/giangtm/baiby/Constants.java
+++ app/src/main/java/com/example/giangtm/baiby/Constants.java
@@ -1,6 +1,7 @@
 package com.example.giangtm.baiby;
 
 public class Constants {
+    //this is test for git commit
     public static final int INCOMMINGMESSAGE = 0;
     public static final int OUTCOMMINGMESSAGE = 1;
 }
